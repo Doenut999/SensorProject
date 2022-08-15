aecee975c3a74573aa008791bc60d8e9e17bc2c0
Alfred Lotsu <alfrdlotsu2@gmail.com>
2022-08-15 16:16:09 +0000

    sensor list started

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import LogSignWelcome from "./pages/LogSignWelcome";
 import GlobalStyle from "./components/GlobalStyle";
+import SensorList from "./pages/SensorList";
 
 const App = ()  =>{
 
   return (
     <div className="App">
-        <GlobalStyle />
-      <LogSignWelcome />
-
+      {/*  <GlobalStyle />*/}
+      {/*<LogSignWelcome />*/}
+        <SensorList />
     </div>
   );
 }
---
+++ src/pages/SensorList.js
@@ -0,0 +1,97 @@
+import FuelPressureSensor from "../assets/FuelPressureSensor.jpg"
+import CoolantLevelSensor from "../assets/CoolantLevelSensor.jpg"
+import NoxSensor from "../assets/NoxSensor.jpg"
+import OilPressureSensor from "../assets/OilPressureSensor.jpg"
+import OilLevelSensor from "../assets/OilLevelSensor.webp"
+import KnockSensor from "../assets/KnockSensor.webp"
+import OxygenSensor from "../assets/OxygenSensor.jpg"
+import BoostPressureSensor from "../assets/BoostPressureSensor.jpg"
+import CrankshaftPositionSensor from "../assets/CrankshaftPositionSensor.jpg"
+import CoolantTempSensor from "../assets/CoolantTempSensor.jpg"
+const SensorList = () => {
+
+    return (
+        <>
+            <div className="sensor">
+                <div className="sensor-img">
+                    <img src={FuelPressureSensor} alt="fuel pressure sensor"/>
+                </div>
+                <div className="sensor-info">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, id minima quas rem repellat suscipit.
+                </div>
+            </div>
+            <div className="sensor">
+                <div className="sensor-img">
+                    <img src={CoolantLevelSensor} alt="coolant-level-sensor"/>
+                </div>
+                <div className="sensor-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur et harum non nulla voluptas.
+                </div>
+            </div>
+            <div className="sensor">
+                <div className="sensor-img">
+                    <img src={NoxSensor} alt="nox sensor"/>
+                </div>
+                <div className="sensor-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi eos itaque laborum maxime pariatur possimus!
+                </div>
+            </div>
+            <div className="sensor">
+                <div className="sensor-img">
+                    <img src={OilLevelSensor} alt="oil level sensor"/>
+                </div>
+                <div className="sensor-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto debitis laudantium saepe unde vitae voluptas!</div>
+            </div>
+            <div className="sensor">
+                <div className="sensor-img">
+                    <img src={OilPressureSensor} alt=""/>
+                </div>
+                <div className="sensor-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam autem cupiditate inventore nihil non reiciendis.
+                </div>
+            </div>
+            <div className="sensor">
+                <div className="sensor-img">
+                    <img src={OxygenSensor} alt=""/>
+                </div>
+                <div className="sensor-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim ipsam quasi quia quo similique voluptatem.
+                </div>
+            </div>
+            <div className="sensor">
+                <div className="sensor-img">
+                    <img src={CoolantTempSensor} alt="coolant temperature sensor"/>
+                </div>
+                <div className="sensor-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A consectetur ipsa minima mollitia perferendis voluptatum.
+                </div>
+            </div>
+            <div className="sensor">
+                <div className="sensor-img">
+                    <img src={KnockSensor} alt="knock sensor"/>
+                </div>
+                <div className="sensor-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi, ipsam maiores molestiae nostrum vero.
+                </div>
+            </div>
+            <div className="sensor">
+                <div className="sensor-img">
+                    <img src={CrankshaftPositionSensor} alt="crankshaft position sensor"/>
+                </div>
+                <div className="sensor-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A asperiores ex nihil perspiciatis saepe ullam.
+                </div>
+            </div>
+            <div className="sensor">
+                <div className="sensor-img">
+                    <img src={BoostPressureSensor} alt="boost pressure sensor"/>
+                </div>
+                <div className="sensor-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum ipsa molestias necessitatibus officia perferendis recusandae voluptate!
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default SensorList
