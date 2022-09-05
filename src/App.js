1c078f22c57b8aeb18f551bebcd666eef0591553
Alfred Lotsu <alfrdlotsu2@gmail.com>
2022-09-05 21:13:10 +0000

    sensor-list-20%

--- src/App.js
+++ src/App.js
@@ -18,18 +18,18 @@ const App = ()  =>{
     <>
         <GlobalStyle />
         <Routes>
-            <Route path="/" element={<AboutPage />} />
-            <Route path="auth" element={<LogSignWelcome />}/>
-            <Route path="list" element={<SensorList />} />
-            //Pages to each Sensors YouTube>
-            <Route path="fuel-sensor" element={<FuelSensorIframe />} />
-            <Route path="oxygen-sensor" element={<OxygenSensor />} />
-            <Route path="coolant-temp-sensor" element={<CoolantTemperature />} />
-            <Route path="oil-level-sensor" element={<OilLevelSensor />} />
-            <Route path="crankshaft-sensor" element={<CrankShaft />} />
-            <Route path="knock-sensor" element={<KnockSensor />} />
-            <Route path="mass-airflow-sensor" element={<MassAirFlow />} />
-            <Route path="throttle-position-sensor" element={<ThrottlePositionSensor />} />
+            {/*<Route path="/" element={<AboutPage />} />*/}
+            {/*<Route path="auth" element={<LogSignWelcome />}/>*/}
+            {/*<Route path="list" element={<SensorList />} />*/}
+            {/*//Pages to each Sensors YouTube>*/}
+            {/*<Route path="fuel-sensor" element={<FuelSensorIframe />} />*/}
+            {/*<Route path="oxygen-sensor" element={<OxygenSensor />} />*/}
+            {/*<Route path="coolant-temp-sensor" element={<CoolantTemperature />} />*/}
+            {/*<Route path="oil-level-sensor" element={<OilLevelSensor />} />*/}
+            {/*<Route path="crankshaft-sensor" element={<CrankShaft />} />*/}
+            {/*<Route path="knock-sensor" element={<KnockSensor />} />*/}
+            {/*<Route path="mass-airflow-sensor" element={<MassAirFlow />} />*/}
+            {/*<Route path="throttle-position-sensor" element={<ThrottlePositionSensor />} />*/}
         </Routes>
     </>
   );
