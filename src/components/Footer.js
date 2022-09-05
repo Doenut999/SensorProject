ec95cc2123f30e7c718fd9ba702e42c457123d65
Alfred Lotsu <alfrdlotsu2@gmail.com>
2022-09-05 17:31:47 +0000

    footer improved

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,10 @@ const Footer = () => {
             <h1 style={{textDecoration: "underline"}}>Contact Us</h1>
             <h3 style={{textDecoration: "underline"}}>Team Members</h3>
             <People>
-                <span><span title="024 450 1927">Samuel Kpornu Dawson</span> - 3603018</span>
-                <span><span title="055 462 9701">Akpabla Andreas</span> - 360218</span>
-                <span><span title="020 121 5534">Osei Sarfo Gideon</span> - 3605018</span>
-                <span><span title="050 441 3325">Kwarteng Peprah Emmanuel</span> - 3603718</span>
+                <span><div><span title="024 450 1927">Samuel Kpornu Dawson</span></div> <div>3603018</div></span>
+                <span><div><span title="055 462 9701">Akpabla Andreas</span></div> <div>3602218</div></span>
+                <span><div><span title="020 121 5534">Osei Sarfo Gideon</span></div><div> 3605018</div></span>
+                <span><div><span title="050 441 3325">Kwarteng Peprah Emmanuel</span></div><div>3603718</div></span>
             </People>
         </Full>
     )
@@ -27,9 +27,26 @@ const Full = styled.footer`
 
 const People = styled.div`
   display: flex;
-  width: 60%;
+  border: 2px solid blueviolet;
+  width: 100%;
+  min-width: 300px;
   flex-direction: column;
   justify-content: space-around;
   align-items: center;
+  & span {
+    width: 100%;
+    display: inline-flex;
+    justify-content: space-around;
+    @media (max-width: 400px){
+      flex-direction: column;
+    }
+     & div {
+       width: 50%;
+       @media (max-width: 400px){
+         width: 100%;
+       }
+     }
+  }
+
 `
 export default Footer
